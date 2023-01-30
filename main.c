d4a88be7fee81cfc6899cbaec327d3806616ae0a
mrmrashu <105367150+mrmrashu@users.noreply.github.com>
2023-01-30 14:30:44 +0530

    Added Invalid Argument function and Comments

--- main.c
+++ main.c
@@ -2,11 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+// The Text buffer that user will input
+char text[100];
+
+// Create a new Entry
 void add_entry(){
 	printf("\nStart Writting...\n");
+    fgets(text,20,stdin);
 }
+
+// Displaying Help Menu
 void help_menu(){
-	printf("Welcome to Help Menu\n");
+	printf("\nWelcome to Help Menu\n");
+}
+
+// Handling invalid arguments
+void invalid_args()
+{
+	printf("Invalid Argument(s), Please use '-h' for more information\n");
 }
 
 int main(int argc, char const *argv[])
@@ -17,16 +30,23 @@ int main(int argc, char const *argv[])
 		for (int i = 1; i < argc; i++)
 		{
 			printf("Argument %d : %s\n", i, argv[i]);
+			// Redirecting to help menu
 			if(! strcmp(argv[i], "-h")) {
 				help_menu();
 			}
+			// Redirecting to New Entry
 			if(! strcmp(argv[i], "-new")) {
 				add_entry();
 			}
+			// Handling any other argument entered other than above
+			else {
+				invalid_args();
+			}
 		}
 	}
+	// Handling no arguments passed
 	if (argc <= 1) {
-		printf("Welcome to Dear Diary, Please use '-h' for more information\n");
+		printf("Please use '-h' for more information\n");
 	}
 	return 0;
 }
